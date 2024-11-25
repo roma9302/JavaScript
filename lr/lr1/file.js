d3d13e2a860f527da6f7d50dc4bdb6600193aac3
Roma <bshs71734@gmail.com>
2024-11-26 01:37:20 +0300

    Lr1

--- lr/lr1/file.js
+++ lr/lr1/file.js
@@ -1,1 +1,96 @@
+//number 1
+var num = 3;
+alert(num)
 
+//number 2
+var a = 10;
+var b = 2;
+alert(a+b)
+alert(a-b)
+alert(a*b)
+alert(a/b)
+
+//number 3
+var c = 15;
+var d = 2;
+result = d+c
+alert(result)
+
+//number 4
+var a = 10;
+var b = 5;
+var c = 2;
+alert(a+b+c)
+
+//number 5
+var a =17;
+var b = 10;
+var c = a-b;
+var d=c;
+alert(d)
+
+//number 6
+var str = "Привет,Мир!";
+alert(str)
+
+//number 7
+var str1='Привет,';
+var str2='Мир!';
+alert(str1+str2)
+
+//number 8
+var name = 'Roma';
+alert('Привет! '+ name)
+
+//number 9
+var age = 16;
+alert('Мне '+age + ' Лет')
+
+//number 10
+var name = prompt('Ваше имя?');
+alert('Ваше имя: '+name);
+
+//num 11
+var num = prompt('num');
+alert(num*num);
+
+//num 12
+var str = 'abcde';
+alert(str[0])
+alert(str[2])
+alert(str[4])
+
+//number 13
+var n='12345';
+alert(n[0]*n[1]*n[2]*n[3]*n[4])
+
+//number 14
+alert('Секунд в месяце ' + 60*60*24*31 )
+alert('Секунд в дне ' + 60*60*24)
+alert('Секунд в часе ' +60*60)
+
+//number 15
+var hour = 5;
+var min = 34;
+var sec = 25;
+alert(hour + ':'+min+':'+sec)
+
+//number 16
+var number = 10;
+alert(number*number)
+
+//number 17
+var num = 47;
+num  += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+
+
+//number 18
+var num = 10;
+num++;
+num++;
+num--;
+alert(num);
