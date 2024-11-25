6bcc7eeff0bf1a9604bac8b2884f82093e21be1f
Roma <bshs71734@gmail.com>
2024-11-26 01:54:49 +0300

    Update file.js

--- lr/lr2/file.js
+++ lr/lr2/file.js
@@ -11,3 +11,21 @@ var a = -1;
 if (a < 0) alert('Верно!'); else alert('Неверно!');
 
 //number 4
+var a = 3;
+if (a >= 0) alert('Верно!'); else alert('Неверно!');
+
+//number 5
+var a = -1;
+if (a <= 0) alert('Верно!'); else alert('Неверно!');
+
+//number 6
+var a = 0;
+if (a != 0) alert('Верно!'); else alert('Неверно!');
+
+//number 7
+var a = 0;
+if (a == 'test' ) alert('Верно!'); else alert('Неверно!');
+
+//number 8
+var a = '1';
+if (a ===  1 ) alert('Верно!'); else alert('Неверно!');
