a334def3e893a46aa73528b6df864e769cbc65b4
Roma <bshs71734@gmail.com>
2024-12-07 11:22:08 +0300

    Update file.js

--- lr/lr5/file.js
+++ lr/lr5/file.js
@@ -68,13 +68,13 @@ if (is_five([1,2,3,6,7,5]) == true){
 var counter = 0;  
 function simple(num){
   for(var i = 1;i<num-1;i++){
-    if (num%i == 1 ||  num/i == 2 ||  num/i == 3  || num/i == 4  || num/i == 5 ||   num/i == 6 || num/i == 7   || num/i == 8 ||  num/i == 9 || num/i == 10){
+    if (num%i == 0){
       counter++
     }
   }
   return counter;
 }
-a = simple(5)
+a = simple(293)
 console.log(a)
 if (a==1){
   console.log('false')
