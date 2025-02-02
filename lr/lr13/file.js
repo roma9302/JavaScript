9332eae2020fad0c8075e381f99e95cc66552f82
Roma <bshs71734@gmail.com>
2025-02-02 19:16:17 +0300

    Update file.js

--- lr/lr13/file.js
+++ lr/lr13/file.js
@@ -1,1 +1,36 @@
+//number 1
+class User{
+  constructor(name,surname,year){
+    this.name = name;
+    this.surname = surname;
+    this.year = year;
+  }
 
+
+  getFullName(){
+    alert(this.name + ' ' + this.surname)
+  }
+
+  
+}
+
+
+class Student extends User{
+  constructor(name,surname,year){
+    super(name,surname,year);
+  }
+  getCourse(){
+    var today = new Date();
+    let year_today = today.getFullYear(); 
+    alert(year_today-Number(this.year))  
+  }
+  
+}
+
+
+user = new Student("Александр","Пушкин","2023")
+alert(user.name)
+alert(user.surname)
+alert(user.year)
+user.getFullName()
+user.getCourse()
